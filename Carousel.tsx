6de04f58b3408db053b20514f7c70cbda68f9281
Viktor Lenard <35898615+viktorlenard@users.noreply.github.com>
2024-11-25 23:16:28 +0000

    Update Carousel.tsx
    Removed custom button component. Wasn't good.

--- Carousel.tsx
+++ Carousel.tsx
@@ -1,8 +1,7 @@
-// VN240716.1
+// VN240716.2
 
 'use client'
 
-import { Button } from '@/components/Button';  // See my Button component, or just use a regular <button>
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import Image from 'next/image'; // Remove if you're not using next.js. Replace <Image> with <img>
 import gsap from 'gsap';
@@ -200,9 +199,11 @@ export const Carousel = ({ content, ...props } : CarouselProps) => {
                     </div>
                 </div>
                 <div className='flex items-center justify-between w-full h-14 bg-zinc-900 py-4'>
-                    <Button onClick={() => prevSlide()} dark={false} size={'sq'} className='text-gray-300 px-2 ml-3'>
-                        <h1 className='text-zinc-900 text-xs w-9'>left</h1>
-                    </Button>
+                    <button onClick={() => prevSlide()} className='bg-light text-dark rounded-sm py-1 px-4 ml-3'>
+                        <svg width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z" fill="#000000" fillRule="evenodd" clipRule="evenodd" stroke="#000000" stroke-width="0.3"></path>
+                        </svg>
+                    </button>
                     <div className='flex flex-col justify-center items-center'>
                         <div className='flex'>
                         {content.map((_, index) => (
@@ -217,9 +218,11 @@ export const Carousel = ({ content, ...props } : CarouselProps) => {
                         ))}
                         </div>
                     </div>
-                    <Button onClick={() => nextSlide()} dark={false} size={'s'} className='text-gray-300 px-2 mr-3'>
-                        <h1 className='text-zinc-900 text-xs w-9'>right</h1>
-                    </Button>
+                    <button onClick={() => nextSlide()} className='bg-light text-dark rounded-sm py-1 px-4 mr-3'>
+                        <svg width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="#000000" fillRule="evenodd" clipRule="evenodd" stroke="#000000" stroke-width="0.3"></path>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
